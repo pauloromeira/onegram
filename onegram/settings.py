34ff0c5f5b456070d636072bf3e05ee1ea9d6dae
Paulo Romeira <paulo@pauloromeira.com>
2018-02-26 22:48:10 -0300

    Add INSTA_DEBUG env variable

--- onegram/settings.py
+++ onegram/settings.py
@@ -6,11 +6,13 @@ from decouple import config
 USERNAME = config('INSTA_USERNAME', default=None)
 PASSWORD = config('INSTA_PASSWORD', default=None)
 
-SSL_INSECURE = True
+DEBUG = config('INSTA_DEBUG', default=False, cast=bool)
+
+SSL_INSECURE = DEBUG
 
 # USER_AGENT = 'fake-useragent'
 
 LOG_SETTINGS = {
     'format': '%(levelname)s:%(name)s:%(funcName)s:%(message)s',
-    'level': logging.DEBUG,
+    'level': logging.DEBUG if DEBUG else logging.INFO,
 }
