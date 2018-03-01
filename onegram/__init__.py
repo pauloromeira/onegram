42c6bd1eb3662317666c8cdcdcd36cdb6d63ade9
Paulo Romeira <paulo@pauloromeira.com>
2018-03-01 01:35:04 -0300

    Add all actions and queries to root init file

--- onegram/__init__.py
+++ onegram/__init__.py
@@ -1,1 +1,11 @@
 from .session import Insta
+
+from .actions import follow, unfollow
+from .actions import like, unlike
+from .actions import comment, uncomment
+from .actions import save, unsave
+
+from .queries import user_info
+from .queries import followers, following
+from .queries import posts, explore
+
