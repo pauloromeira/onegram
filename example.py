463a6399927f52f205e1aa7db3259cdbc3b57dc3
Paulo Romeira <paulo@pauloromeira.com>
2018-03-02 00:28:13 -0300

    Tweak example

--- example.py
+++ example.py
@@ -4,15 +4,18 @@ from itertools import islice
 
 from onegram import Login
 
-from onegram.queries import user_info, post_info
-from onegram.queries import followers, following
-from onegram.queries import posts, likes, comments
-from onegram.queries import explore
+# Queries
+from onegram import user_info, post_info
+from onegram import followers, following
+from onegram import posts, likes, comments
+from onegram import explore
 
-from onegram.actions import follow, unfollow
-from onegram.actions import like, unlike
-from onegram.actions import comment, uncomment
-from onegram.actions import save, unsave
+# Actions
+from onegram import follow, unfollow
+from onegram import like, unlike
+from onegram import comment, uncomment
+from onegram import save, unsave
+
 
 with Login():
     p = next(posts())
