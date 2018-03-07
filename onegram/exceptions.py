7d6419e9db71ae9fedd76951c1ae933c60a0f35c
Paulo Romeira <paulo@pauloromeira.com>
2018-03-06 23:13:15 -0300

    Add exceptions module with todo's

---
+++ onegram/exceptions.py
@@ -0,0 +1,7 @@
+# TODO [romeira]: Login exceptions {06/03/18 23:07}
+# TODO [romeira]: Query/action exceptions {06/03/18 23:08}
+# TODO [romeira]: Session expired exception {06/03/18 23:08}
+# TODO [romeira]: Private user exception/warning {06/03/18 23:09}
+# TODO [romeira]: Not found exception {06/03/18 23:12}
+# TODO [romeira]: Already following/liked/commented? warnings {06/03/18 23:12}
+# TODO [romeira]: Timeout exception {06/03/18 23:12}
