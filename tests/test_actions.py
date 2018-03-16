6ff107527706e2370065b1f05c395804c6743b3d
Paulo Romeira <paulo@pauloromeira.com>
2018-03-16 01:37:49 -0300

    Update TODO's and tweak test_actions

--- tests/test_actions.py
+++ tests/test_actions.py
@@ -5,13 +5,13 @@ from helpers.responses import follow_responses
 from helpers.responses import unfollow_responses
 
 
-def test_follow_username(session, responses):
+def test_follow(session, responses):
     username = 'other'
     user_info_responses(responses, username)
     follow_responses(responses, username)
     follow(username)
 
-def test_unfollow_username(session, responses):
+def test_unfollow(session, responses):
     username = 'other'
     user_info_responses(responses, username)
     unfollow_responses(responses, username)
