7c94b9f08827ef69f391aa4f789ebc640e445937
Paulo Romeira <paulo@pauloromeira.com>
2018-03-15 22:57:15 -0300

    Add test_login_request

--- tests/test_session.py
+++ tests/test_session.py
@@ -1,10 +1,15 @@
 from onegram.constants import DEFAULT_COOKIES
 
 
-def test_session(session):
+def test_session_attrs(session):
     cookies = {'csrftoken': 'token'}
     cookies.update(DEFAULT_COOKIES)
     assert cookies == session.cookies
     assert session.username == 'username'
     assert session.user_id == '1'
 
+
+def test_login_request(responses, session):
+    request = responses.last_request
+    assert request.text == 'username=username&password=password'
+    assert request.headers['X-CSRFToken'] == 'token'
