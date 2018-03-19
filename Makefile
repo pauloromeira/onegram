145da8ea771f00e811badddcf1e7750d2fb651dd
Paulo Romeira <paulo@pauloromeira.com>
2018-03-19 03:00:23 -0300

    Add setup.py and update Makefile

--- Makefile
+++ Makefile
@@ -59,3 +59,15 @@ coverage: ## check code coverage quickly with the default Python
 	@coverage report -m
 	@coverage html
 	@$(BROWSER) htmlcov/index.html
+
+release: clean ## package and upload a release
+	python setup.py sdist upload
+	python setup.py bdist_wheel upload
+
+dist: clean ## builds source and wheel package
+	python setup.py sdist
+	python setup.py bdist_wheel
+	ls -l dist
+
+install: clean ## install the package to the active Python's site-packages
+	python setup.py install
---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+
+requires = [
+    'fake-useragent>=0.1.10',
+    'jmespath>=0.9.3',
+    'python-decouple>=3.1',
+    'requests>=2.18.4',
+    'sessionlib>=0.2.0',
+    'tenacity>=4.9.0'
+]
+
+setup(
+    name='onegram',
+    version='1.0.0',
+    description='A simplistic api-like instagram bot powered by requests',
+    url='https://github.com/pauloromeira/onegram',
+    author='Paulo Romeira',
+    author_email='paulo@pauloromeira.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+    packages=find_packages(exclude=['tests']),
+    install_requires=requires
+)
