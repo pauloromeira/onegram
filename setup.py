53e2925fdd21f2d2e492f9dac73b05e879fbd8fc
Paulo Romeira <paulo@pauloromeira.com>
2018-04-28 17:37:36 -0300

    Release 1.2.3

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ requires = [
 
 setup(
     name='onegram',
-    version='1.2.2',
+    version='1.2.3',
     description='A simplistic api-like instagram bot powered by requests',
     url='https://github.com/pauloromeira/onegram',
     author='Paulo Romeira',
