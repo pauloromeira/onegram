34e7f348f583c99abb74308d554c7ade4a9f6b1d
neFAST <neFAST@users.noreply.github.com>
2018-05-27 11:11:39 +0200

    better handle non existing users

--- onegram/queries.py
+++ onegram/queries.py
@@ -81,7 +81,11 @@ def _user_id(session, user):
     if user is None:
         raise ValueError('Invalid user')
 
-    return user_info(session, user)['id']
+    info = user_info(session, user)
+    if info is None:
+        raise ValueError('Invalid user')
+    else:
+        return info['id']
 
 def _post_id(post):
     if isinstance(post, dict):
